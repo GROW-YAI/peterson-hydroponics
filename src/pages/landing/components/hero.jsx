88699eef6c733029900f96e5e286f46c9be2c46f
Tilopsy <bettylizzie25@gmail.com>
2025-02-21 07:32:53 +0000

    hero section set up

--- src/pages/landing/components/hero.jsx
+++ src/pages/landing/components/hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import heroVideo from '../../../assets/Herovideo.mp4'; // Ensure this path is correct
 
+
 const Hero = () => {
   return (
-    <div className="relative flex h-screen overflow-hidden">
+    <div className="relative flex h-screen ">
       <video
         src={heroVideo}
         autoPlay
@@ -11,15 +12,15 @@ const Hero = () => {
         muted
         className="absolute top-0 left-0 w-full h-full object-cover"
       />
-      <div className="relative z-10 mx-auto w-fit flex flex-col items-start gap-y-4 p-20 bg-black bg-opacity-50">
+      <div className="relative z-10 mx-auto w-fit flex flex-col items-start gap-y-4 p-20 bg-black bg-opacity-35">
         <div className="flex flex-col text-5xl text-white">
-          <span className="font-bold">Tilopsy</span>
-          <span className="font-thin">Your Best Bet For Events<br /></span>
+          <span className="font-bold">Peterson Hydroponics</span>
+          <span className="font-thin">Your Food Solutions<br /></span>
         </div>
-        <p className="w-80 text-white">
-          What’s that, you say? He inhaled a painting?? The man must be enormous! Not quite. Wigan’s works are ‘micro-sculptures’, so tiny they must be viewed through a microscope.
+        <p className="w-80 text-white ">
+        Let food be thy medicine and medicine be thy food!
         </p>
-        <button className="#E90074 px-6 py-2 rounded-full drop-shadow-lg text-cyan-800">
+        <button className="#E90074 py-2 rounded-full drop-shadow-lg text-green-400 font-bold">
           DISCOVER
         </button>
       </div>
@@ -27,4 +28,7 @@ const Hero = () => {
   );
 }
 
+
 export default Hero;
+
+
