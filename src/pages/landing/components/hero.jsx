f086f4051ab91eeb59993cb65cc47e00e2cfe117
Tilopsy <bettylizzie25@gmail.com>
2025-03-03 08:23:46 +0000

    hero section responsiveness

--- src/pages/landing/components/hero.jsx
+++ src/pages/landing/components/hero.jsx
@@ -3,7 +3,7 @@ import heroVideo from '../../../assets/Herovideo.mp4'; // Ensure this path is co
 
 const Hero = () => {
   return (
-    <div className="relative flex h-screen">
+    <div className="relative flex h-screen items-center justify-center">
       {/* Background Video */}
       <video
         src={heroVideo}
@@ -14,23 +14,20 @@ const Hero = () => {
       />
       
       {/* Hero Content */}
-      <div className="relative z-10 mx-auto w-fit flex flex-col items-center gap-y-4 p-20 bg-black bg-opacity-35 text-center">
-        <div className="text-white text-5xl">
+      <div className="relative z-10 w-full flex flex-col items-center gap-y-4 px-6 py-12 bg-black bg-opacity-35 text-center">
+        <div className="text-white text-4xl sm:text-5xl">
           <span className="font-bold">Peterson Hydroponics</span>
           <br />
-          <span className="font-thin">Your Food Solutions</span>
+          <span className="font-thin">Your Organic Food Solutions</span>
         </div>
         <p className="w-80 text-white">
           Let food be thy medicine and medicine be thy food!
         </p>
         
         {/* Welcome Button */}
-        <button className="#E90074 py-2 rounded-full drop-shadow-lg text-green-400 font-bold text-4xl">
+        <button className="py-2 px-6 rounded-full drop-shadow-lg text-green-400 font-bold text-3xl sm:text-4xl">
           WELCOME
         </button>
-        {/* <button className=" hover:bg-green-600 text-green-400 text-2xl font-bold py-3 px-8 rounded-full drop-shadow-lg">
-          WELCOME
-        </button> */}
       </div>
     </div>
   );
